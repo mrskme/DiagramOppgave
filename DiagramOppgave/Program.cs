c8501b40bf8f249a872793454589a75e766b5dab
mrskme <eiriksb@getacademy.no>
2021-02-11 13:06:03 +0100

    Initial commit

---
+++ DiagramOppgave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiagramOppgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
